0c0cf56b8780d238148fbc9a256024c34c8ded7c
joshdfg <slimmy0310@gmail.com>
2024-08-09 11:30:04 -0400

    feat: updates

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use scraper::Html;
 mod lib;
 
 fn parse_html_to_ast(html: &str) -> Html {
-    // Parse the HTML string into a DOM tree
     Html::parse_document(html)
 }
 
@@ -16,7 +15,4 @@ fn main() {
         .iter()
         .map(|html| parse_html_to_ast(html))
         .for_each(|dom| println!("{:#?}", dom));
-
-    // `dom` now contains the root of the parsed HTML tree
-    // You can traverse and manipulate the DOM tree as needed
 }
